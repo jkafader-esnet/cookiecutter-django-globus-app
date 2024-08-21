772bf9ee836400b677bddb28c04f3bee890d4483
Johanna Lee <johannalee@berkeley.edu>
2024-08-20 22:53:09 -0700

    updating transfer react page 4

--- {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/Home.jsx
+++ {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/Home.jsx
@@ -61,11 +61,11 @@ const Home = (props) => {
   return (
     <div id='transfer-home' className='container-fluid mt-4'>
       <div className='row'>
-        <div className='col-8' style={{marginLeft: 'auto', marginRight: 'auto'}}>
+        <div className='col-10'>
           <PortalEndpoint />
         </div>
 
-        <div className='col-8' style={{marginLeft: 'auto', marginRight: 'auto'}}>
+        <div className='col-10'>
           {/* 
             Endpoints will render here when the SearchEndpointLink is clicked below. 
             See App.jsx nested routing for more details. 
