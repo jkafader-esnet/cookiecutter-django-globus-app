794cbeb1fa15237ae62a1db176c5dec70be424ed
Johanna Lee <johannalee@berkeley.edu>
2024-08-22 15:45:04 -0700

    editing file browser

--- {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/PortalEndpoint.jsx
+++ {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/PortalEndpoint.jsx
@@ -208,7 +208,7 @@ const PortalEndpoint = (props) => {
         {portalCollection && portalCollection['DATA'].length > 0 &&
           portalCollection['DATA'].map((item) => {
             return (
-              <div key={`${item['last_modified']}-${item['name']}`} style= {{flexBasis: '20%', padding: '8px', boxSizing: 'border-box'}}>
+              <div key={`${item['last_modified']}-${item['name']}`} className = 'col-2' style= {{flexBasis: '20%', padding: '8px', boxSizing: 'border-box'}}>
                 <div style= {{cursor:'pointer'}}>
                     {item['type'] == 'dir' ? (
                       <a
