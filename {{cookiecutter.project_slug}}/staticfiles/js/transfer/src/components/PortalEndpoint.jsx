f4a64eec78dcee79e66283709505179ca2937216
Johanna Lee <johannalee@berkeley.edu>
2024-08-22 15:34:18 -0700

    editing file browser

--- {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/PortalEndpoint.jsx
+++ {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/PortalEndpoint.jsx
@@ -201,14 +201,14 @@ const PortalEndpoint = (props) => {
       <h5>Browsing Portal Source Node</h5>
 
       <div className='file-browser border mb-4 pb-4 pl-2 pt-2 rounded'>
-        <button className='btn btn-primary btn-sm mb-2' onClick={handleBackClick} style={{ display: 'block' }}>
+        <button className='btn btn-primary btn-sm mb-2' onClick={handleBackClick}>
           Back
         </button>
 
         {portalCollection && portalCollection['DATA'].length > 0 &&
           portalCollection['DATA'].map((item) => {
             return (
-              <div key={`${item['last_modified']}-${item['name']}`} style= {{flexBasis: '100px', padding: '8px', boxSizing: 'border-box'}}>
+              <div key={`${item['last_modified']}-${item['name']}`} style= {{flexBasis: '20%', padding: '8px', boxSizing: 'border-box'}}>
                 <div style= {{cursor:'pointer'}}>
                     {item['type'] == 'dir' ? (
                       <a
