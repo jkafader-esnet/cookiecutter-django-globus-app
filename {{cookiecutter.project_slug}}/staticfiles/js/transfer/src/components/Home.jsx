76c31ec63dc8200b321d44f1e0ad941df9fcbd8f
Johanna Lee <johannalee@berkeley.edu>
2024-08-28 20:31:10 -0700

    fixing transfer state 1

--- {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/Home.jsx
+++ {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/Home.jsx
@@ -69,7 +69,7 @@ const Home = (props) => {
             className='form-control'
             placeholder='Search'
             type='text'
-            value={searchEndpoints || endpointSearchText}
+            value={searchEndpoint['display_name'] || endpointSearchText}
             onChange={handleEndpointSearchTextChange}
             onKeyDown={handleSearchEndpoints}
           />
