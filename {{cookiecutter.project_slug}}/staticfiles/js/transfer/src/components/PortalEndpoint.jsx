7a48af04e3c14db60b8cbe0ba09c2d27c9a55dbd
James Kafader <jkafader@es.net>
2025-02-10 16:47:30 -0800

    add CSS so transfer queue items are stylable

--- {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/PortalEndpoint.jsx
+++ {{cookiecutter.project_slug}}/staticfiles/js/transfer/src/components/PortalEndpoint.jsx
@@ -251,10 +251,10 @@ const PortalEndpoint = (props) => {
       
       <h5>Transfer Queue</h5>
       <div className = 'border mb-4 pb-4 pl-2 pt-2 rounded'>
-        <ul>
+        <ul className='transfer-queue'>
         {selectedPortalItems.length > 0 ? (
           selectedPortalItems.map((portalItem, index) => (
-            <li key = {index}> {portalItem['name'] }</li>
+            <li className='transfer-queue-item' key = {index}> {portalItem['name'] }</li>
           ))
           ) : (
           <p>No items available to display</p>
